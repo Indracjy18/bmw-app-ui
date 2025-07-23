17c34f885e6fef0836bbe3aea06c36613802daa5
Indracjy18 <indracahyajaty@gmail.com>
2025-07-24 03:41:26 +0700

    fix url

--- src/service/api/axiosAuth.jsx
+++ src/service/api/axiosAuth.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "bmw-app-be.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL || "bmw-app-be.up.railway.app/api";
 
 export const axiosAuth = axios.create({
   baseURL: API_URL,
--- src/service/api/karyawanService.jsx
+++ src/service/api/karyawanService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "bmw-app-be.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL || "bmw-app-be.up.railway.app/api";
 
 // ======================================================================
 // Interceptor Axios untuk Penanganan Error 401 Global

036971abd9f530f270f7c17f0a4ac0509f366654
Indracjy18 <indracahyajaty@gmail.com>
2025-07-23 15:04:24 +0700

    Fix blank screen: Add base defineconfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/login",
+  base: "/",
   build: {
     rollupOptions: {
       treeshake: {
